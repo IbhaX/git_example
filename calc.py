8a154fb99dbcf623937219ed4005a76a59b944c8
Albin Anthony <albin.anthony@onedatasoftware.com>
2023-10-14 13:36:26 +0530

    swapped methodas

--- calc.py
+++ calc.py
@@ -1,8 +1,8 @@
 
 
 
-def add(a,b):
-    return a + b
+def mul(a,b):
+    return a * b
 
 def sub(a,b):
     return a - b
@@ -11,5 +11,5 @@ def sub(a,b):
 def div(a,b):
     return a / b
 
-def mul(a,b):
-    return a*b
+def add(a,b):
+    return a+b
